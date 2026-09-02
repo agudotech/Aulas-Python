1b3fb8b9ddcb4d57a9d82830e5ea871ada96b8d3
agudotech <antonioagudo0905@gmail.com>
2026-09-02 14:40:36 -0300

    feat: conclui atividade de Python

--- atividades/Atividade2.py
+++ atividades/Atividade2.py
@@ -2,4 +2,9 @@ print("✦ Bem-vindo(a) ao Formulário de Elegibilidade ✦ ")
 print()
 nome = str(input("Digite seu nome: "))
 idade = int(input("Digite sua idade: "))
-pergunta = input("Você tem plano de saúde? ")
+saude = bool(input("Você tem plano de saúde? "))
+print()
+ 
+aceito = idade >= 18 and idade < 60 and saude 
+
+print("Seu nome é", nome, "\nsua idade é:", idade, "\nVocê foi aceito?", aceito)
